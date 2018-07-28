82d775d71151e3d434473b8a0a9d53653690fa88
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2018-07-28 19:28:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ inc = n => n + 1
 dec = n => n - 1
 makeInt = n => parseInt(n)
 preserveDecimal = n => parseFloat
+
+
+module.exports = {
+  add = (a, b) => a + b
+subtract = (a, b) => a - b
+multiply = (a, b) => a * b
+divide = (a , b) => a / b
+inc = n => n + 1
+dec = n => n - 1
+makeInt = n => parseInt(n)
+preserveDecimal = n => parseFloat
+}
