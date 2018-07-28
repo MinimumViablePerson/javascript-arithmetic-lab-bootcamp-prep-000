83a425beae95d33da4f15d66f80662dd18de0b79
Nicolas Marcora <nicolas.marcora@yahoo.co.uk>
2018-07-28 19:27:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-add
-subtract
-multiply
-divide
-inc
-dec
-makeInt
-makeFloat
+add = (a, b) => a + b
+subtract = (a, b) => a - b
+multiply = (a, b) => a * b
+divide = (a , b) => a / b
+inc = n => n + 1
+dec = n => n - 1
+makeInt = n => parseInt(n)
+preserveDecimal = n => parseFloat(n)
